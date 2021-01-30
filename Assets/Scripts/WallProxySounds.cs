9317802407b397ecccdfa77c426693b32757a714
Carter <nathancarterwilliams@gmail.com>
2021-01-30 10:45:43 -0600

    imported audio

---
+++ Assets/Scripts/WallProxySounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+/*
+ * Author: cwilliams@filamentgames.com
+ *
+ * Purpose:
+ * 
+ *
+*/
+public class WallProxySounds : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    private void Awake()
+    {
+        StartCoroutine(UpdateRoutine());
+    }
+
+    IEnumerator UpdateRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(.1f);
+            
+        }
+    }
+}
