68f24a725218f2d366961249ac9d9d1e33b7af27
Carter <nathancarterwilliams@gmail.com>
2021-01-30 19:37:21 -0600

    bump effect fixes

--- Assets/Scripts/Player/BumpSoundEffect.cs
+++ Assets/Scripts/Player/BumpSoundEffect.cs
@@ -14,12 +14,40 @@ public class BumpSoundEffect : MonoBehaviour
     [SerializeField]
     AudioSource sound;
 
+    float maxVolume;
+
+
+    void Awake()
+    {
+        StartCoroutine(UpdateRoutine());
+        maxVolume = sound.volume;
+    }
+
     void OnCollisionEnter(Collision other)
     {
         if(!other.collider.isTrigger)
         {
             if(!sound.isPlaying)
-                sound.Play();
+            {
+                var dot = Vector3.Dot(other.contacts[0].normal,transform.forward);
+                sound.volume = Mathf.Lerp(0,maxVolume,Mathf.Abs(dot));
+            }
+        }
+    }
+
+    IEnumerator UpdateRoutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+
+            if(Input.GetKey(KeyCode.Mouse0))   
+            {        
+                if(Physics.Raycast(transform.position,transform.forward,.5f,255,QueryTriggerInteraction.Ignore))
+                {
+                    sound.Play();
+                }
+            }             
         }
     }
 }
